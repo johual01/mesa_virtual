6277f067834b183b66617006b2c728b3392b5131
Johual01 <pereirajesusbenja@gmail.com>
2025-01-06 21:31:39 -0300

    fix prevent default

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,20 +5,17 @@ import { rollDiceString } from '../../../diceLogic';
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [submittedValue, setSubmittedValue] = useState('');
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setSubmittedValue(JSON.stringify(rollDiceString(inputValue)));
-  };
+  function submit(){
+    setSubmittedValue(JSON.stringify(rollDiceString(inputValue)))
+  }
   return (
     <main>
-      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type="submit">Submit</button>
-      </form>
+        <button onClick={submit}>Submit</button>
       {submittedValue && <p>Submitted: {submittedValue}</p>}
     </main>
   );
