6570298159cbf14e1f4143d3054359e3e82cc62e
Johual01 <pereirajesusbenja@gmail.com>
2025-03-13 23:19:17 -0300

    feat (style) enhance layout and styling for better responsiveness and structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,10 @@
+"use client"
 
-import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-
-export const metadata: Metadata = {
-  title: "Digital Twenty",
-  description: "Simplificando tus juegos de rol de mesa favoritos",
-};
+import { AuthProvider } from "@/context/auth";
 
 export default function RootLayout({
   children,
@@ -18,16 +14,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar></Navbar>
-          {children}
-          <Footer></Footer>
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", maxHeight: "100vh" }}>
+              <Navbar />
+              <main style={{ flex: 1, display: "flex", flexDirection: "column" }}>{children}</main>
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </AuthProvider>
+        
       </body>
     </html>
   );
--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -12,22 +12,11 @@ const Footer = ({
   copyright = "© 2025 Copyright. All rights reserved.",
 }: FooterProps) => {
   return (
-    <section className="py-32">
-      <div className="container">
+    <section>
+      <div>
         <footer>
-          {/*<div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
-            <div className="col-span-2 mb-8 lg:mb-0">
-              <div className="flex items-center gap-2 lg:justify-start">
-                <p className="text-xl font-semibold">{title}</p>
-              </div>
-              <p className="mt-4 font-bold">{tagline}</p>
-            </div>
-            <div>
-                ### Redes sociales
-            </div>
-          </div>*/}
-          <div className="mt-24 flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
-            <div>{copyright}</div>
+          <div className="flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
+            <div className="pd-3em">{copyright}</div>
           </div>
         </footer>
       </div>
